f8b9a0cf8bcf0e4d96c3ea61fc758e0a5e2d6edf
gon_fox <try.to.avoid@gmail.com>
2020-04-02 16:55:30 +0900

    Bキーでボールを生成する機能追加

---
+++ Assets/Script/CreateBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject obj = (GameObject)Resources.Load("Prefab/Ball");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            GameObject obj = (GameObject)Resources.Load("Prefab/Ball");
+
+            Instantiate(obj, new Vector3(0.1f, 0.8f, 0.2f), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Script/shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public float Speed = 0;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector3(0, 0, Speed);
+            
+        }
+    }
+
+}
+
