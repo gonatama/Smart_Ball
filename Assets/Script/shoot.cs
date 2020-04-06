9208732aa6116ad36079f7cc4fa3c422b08ea7e2
gon_fox <try.to.avoid@gmail.com>
2020-04-06 19:08:08 +0900

    発射機能で一つずつボールを発射可能になりました。 ボールを発射する際にエラー：Rigidbody rb が割り当てられていない。

--- Assets/Script/shoot.cs
+++ Assets/Script/shoot.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class shoot : MonoBehaviour
 {
     public float Speed = 0.0f;
-    //Rigidbody rb;
-    bool chamber = false;
-    GameObject cap;
+    public Rigidbody rb;
+    bool chamber_in = false;
+    //bool chamber_out = true;
+    GameObject cap,obj;
     private Vector3 Cap_point;
+    private string BallName;
     // Start is called before the first frame update
     void Start()
     {
         cap = GameObject.Find("chamber_cap");
         Debug.Log(cap.tag);
+        Rigidbody rb = this.transform.GetComponent<Rigidbody>();
         //Cap_point = new Vector3(cap.transform.localPosition.x, cap.transform.localPosition.y, cap.transform.localPosition.z);
         //cap_point = cap.transform.localPosition;
         Cap_point = new Vector3(cap.transform.position.x, cap.transform.position.y, cap.transform.position.z + 0.2f);
@@ -28,23 +31,24 @@ public class shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (this.chamber == true)
+        
+        if (this.chamber_in == true)
         {
-
+            //chamber_out = fasle;
             if (Input.GetKey("a"))
             {
-                this.transform.position = Cap_point;
 
                 this.Speed += 10.0f;
                 Debug.Log("発射可能");
-            Debug.Log(Cap_point.ToString("F4"));
+                Debug.Log(Cap_point.ToString("F4"));
 
-        }
-        if (Input.GetKeyUp("a"))
+            }
+            if (Input.GetKeyUp("a"))
             {
+                this.transform.position = Cap_point;
+                this.chamber_in = false;
                 //this.rb.velocity = new Vector3(0, 0, Speed);
-                //this.rb.AddForce(new Vector3(0, 0, 1) * this.Speed);
+                this.rb.AddForce(new Vector3(1, 0, 1) * this.Speed);
                 Debug.Log("発射！");
                 //this.Speed = 0.0f;
             }
@@ -55,16 +59,20 @@ public class shoot : MonoBehaviour
     {
         if (collision.gameObject.name == "chamber_wall")
         {
+
             Debug.Log("発射位置へ接触しました.");
+            Debug.Log(this.name);
+            //BallName = this.name;
             //this.transform.position = new Vector3(Cap_point.transform.position.x, Cap_point.transform.position.y, Cap_point.transform.position.z);
             //this.transform.position = Cap_point;
             //Debug.Log(Cap_point.ToString("F4"));
             //Debug.Log(this.transform.position.ToString("F4"));
 
             //Debug.Log("移動完了");
-            if (chamber == false)
-                chamber = true;
-            }
-        }
-    }
+            if (this.chamber_in == false)
+                this.chamber_in = true;
 
+        }
+
+    }
+}
