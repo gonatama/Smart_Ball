ab0149e92b5d77dfc4225fc86e9185a070b23374
gon_fox <try.to.avoid@gmail.com>
2020-04-07 15:59:07 +0900

    Assetsフォルダ内で更新されていなかったものをコミット

---
+++ Assets/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int BallCreateNum = 0;
+    public static GameManager instance = null;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BallCreateNum += 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (BallCreateNum > 0)
+        {
+            GameObject CreateBall = GameObject.Find("CreateBall");
+            CreateBall obj = CreateBall.GetComponent<CreateBall>();
+            obj.num +=  BallCreateNum;
+            BallCreateNum = 0;
+        }
+    }
+}
---
+++ Assets/Script/Detection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "ball")
+        {
+            Debug.Log("通過しました。");
+            Debug.Log(name);
+            Destroy(collision.gameObject);
+        }
+    }
+}
