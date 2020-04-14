dfb485324b112b47e9fb0c86f0879c8350f6e496
gon_fox <try.to.avoid@gmail.com>
2020-04-14 11:11:14 +0900

    ボールを300個のなかで使いまわせるように機能を変更

--- Assets/Script/CreateBall.cs
+++ Assets/Script/CreateBall.cs
@@ -35,103 +35,119 @@ public class CreateBall : MonoBehaviour
             num += 15;
         }
 
-        if (BallActive() == false)
+        //if (BallActive() == false)
+        //{
+        //    Debug.Log("初期化条件を満たしました");
+        //    num = 0;
+        //    //cnt = 0;
+        //    return;
+        //}
+
+        int j = BallActive();
+        n = 0;
+
+        if(j < num)
         {
-            Debug.Log("初期化条件を満たしました");
+            stop = true;
+            j = 0;
             num = 0;
-            //cnt = 0;
-            return;
         }
+        else
+        {
+            stop = false;
+            j = 0;
+        }
+
 
         if (stop == false)
         {
-            if (num > 0)
-            {
-                int localcnt = 0;
-                localcnt = cnt + num;
+                //int localcnt = 0;
+                //localcnt = cnt + num;
 
-                if (localcnt > 300)
+                //if (localcnt > 300)
+                //{
+                //    cnt = 0;
+                //    num = localcnt - num;
+                //}
+
+
+
+
+            for (int i = 0; i < num; i++)
+            {
+                    //if()
+                    Debug.Log(cnt);
+                if (cnt + i >= 300)
                 {
                     cnt = 0;
-                    num = localcnt - num;
+                    num = 0;
+                    return;
                 }
 
 
-
-
-                for (int i = 0; i < num; i++)
-                {
-                    //if()
-                    Debug.Log(cnt);
-
-                    GameObject obj = BallList[cnt + i];
+                GameObject obj = BallList[cnt + i];
                     if (obj.activeSelf == false)
                     {
                         obj.SetActive(true);
                         obj.transform.position = CreateBallPoint;
+
                         
                     }
                     else
                     {
                         i--;
                         cnt++;
-                        Debug.Log(cnt);
+                        //Debug.Log(cnt);
                     }
 
-                    //if (cnt == 300)
-                    //{
-                    //    Debug.Log("初期化条件１");
-                    //}
-                }
-                cnt += num;
-                num = 0;
             }
-            else
-            {
-                return;
-            }
+                //cnt += num;
+
+            num = 0;
         }
     }
 
 
 
-    //void BallActive(int num)
-    //{
-    //    GameObject obj = BallList[num];
 
-    //    if (obj.activeSelf == false)
+    //bool BallActive()
+    //{
+    //    foreach (Transform child in transform)
     //    {
-    //        Debug.Log("false");
+    //        if (child.gameObject.activeSelf == true)
+    //        {
+    //            n++;
+    //            //Debug.Log(n);
+    //        }
     //    }
-    //    else if(obj.activeSelf == true)
+
+    //    if(n >= 299)
     //    {
-    //        Debug.Log("true");
+    //        n = 0;
+    //        stop = true;
+    //        return false;
+
+    //    }
+    //    else
+    //    {
+    //        n = 0;
+    //        stop = false;
+    //        return true;
     //    }
     //}
 
-    bool BallActive()
+    int BallActive()
     {
         foreach (Transform child in transform)
         {
-            if (child.gameObject.activeSelf == true)
+            if (child.gameObject.activeSelf == false)
             {
                 n++;
-                //Debug.Log(n);
+
             }
         }
-
-        if(n >= 299)
-        {
-            n = 0;
-            stop = true;
-            return false;
-
-        }
-        else
-        {
-            n = 0;
-            stop = false;
-            return true;
-        }
+        Debug.Log(n);
+        return n;
     }
+
 }
