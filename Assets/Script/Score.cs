dbd59367ae3c5bb95404552256347e2db002951b
gon_fox <try.to.avoid@gmail.com>
2020-04-20 10:58:56 +0900

    釘のphysicMaterial追加 ゲームオーバーシーンに最高ストック数を表示する機能を追加

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public GameObject obj = null;
+    private Text score_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score_text = obj.GetComponent<Text>();
+        SceneSystem.Score = PlayerPrefs.GetInt("SCORE", 0);
+        Debug.Log(SceneSystem.Score);
+        score_text.text = "GameOver\n"+"最高ボールストック数:" + SceneSystem.Score;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
